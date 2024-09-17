ff27caba8f6a365d96457fb624ff72c323c18093
Raphael Acilo <181692343+rphl-a@users.noreply.github.com>
2024-09-17 23:45:40 +0800

    Quartz sync: Sep 17, 2024, 11:45 PM

---
+++ quartz-startup.bat
@@ -0,0 +1,40 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+call git pull
+call npx quartz update
+call npx quartz sync
+cls
+echo Would you like to build local? 5 seconds until exit (Y/N)
+CHOICE /T 5 /C YN /D N
+set INPUT=%ERRORLEVEL%
+if %INPUT%==1 echo Building local...&goto yes
+if %INPUT%==2 goto no
+:yes
+call npx quartz build --serve
+goto no
+:no
+exit
